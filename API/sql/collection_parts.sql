64f8ae643535e4ccb6d5ff44a47e60a2812474d6
Torsten Petersdorf <github@tpete.de>
2014-12-03 20:52:06 +0100

    Change foreign key relation on collections/collection_parts: collections.movie_db_id -> collection_parts.collection_id

--- API/sql/collection_parts.sql
+++ API/sql/collection_parts.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `collection_parts` (
  `COLLECTION_ID` int(11) NOT NULL,
  `MOVIE_ID` int(11) NOT NULL,
  FOREIGN KEY (`COLLECTION_ID`) 
-        REFERENCES `collections`(id)
+        REFERENCES `collections`(movie_db_id)
         ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
