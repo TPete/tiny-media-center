9adf0fd153d7fe2a056ae358ae79f30d848c4cde
Torsten Petersdorf <github@tpete.de>
2014-12-06 18:21:46 +0100

    Fix styling of category selection and category overview.

--- FrontEnd/templates/categoryOverview.php
+++ FrontEnd/templates/categoryOverview.php
@@ -1,12 +1,9 @@
-<div class='category-overview-wrapper'>
-	<div class='category-overview'>
-	<?php foreach($overview as $data){?>
-		<a href="<?php echo $data["folder"];?>" class="sub-category" title="<?php echo $data["title"];?>">
-			<img class='thumbnail' src='<?php echo $data["thumbUrl"];?>'>
-			<span><?php echo $data["title"];?></span>
-		</a>
-	<?php }?>
-	<br class="clear">
-	</div>
-	<br class='clear'>
+<div class='category-overview'>
+<?php foreach($overview as $data){?>
+	<a href="<?php echo $data["folder"];?>" class="sub-category" title="<?php echo $data["title"];?>">
+		<img class='thumbnail' src='<?php echo $data["thumbUrl"];?>'>
+		<span><?php echo $data["title"];?></span>
+	</a>
+<?php }?>
+<br class="clear">
 </div>
